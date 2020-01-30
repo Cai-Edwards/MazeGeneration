0a10c30a4bb0bc8ed3aa16a92736a26c10e69008
raahweng <55945020+raahweng@users.noreply.github.com>
2020-01-30 13:32:35 +0000

    First time working

--- maze.py
+++ maze.py
@@ -1,91 +1,129 @@
 from random import choice
+import pygame, time
 
 class Maze:
+
     def __init__(self, size):
         self.size = size
-        self.maze = [[Node(x, y) for x in range(size)] for y in range(size)]
+        self.maze = self.make_maze()
+
+    def make_maze(self):
+
+        maze = []
+        for y in range(self.size):
+            maze.append([])
+
+            for x in range(self.size):
+                edges = 0b0000
+                if y == 0: edges = edges | 0b1000
+                if x == 0: edges = edges | 0b0001
+                if y == self.size-1: edges = edges | 0b0010
+                if x == self.size-1: edges = edges | 0b0100
+                
+                maze[y].append(Node(0b1111, edges, x, y))
+
+        return maze                
 
 class Node:
-    def __init__(self, x, y):
-        self.wall = True
+
+    def __init__(self, walls, edges, x, y):
+        self.walls = walls
+        self.edges = edges
+        self.travelled = False
         self.x = x
         self.y = y
 
-    def next_point(self):
-        self.wall = False
-        
-def adjacent(maze, current_pos):
-    adj = []
 
-    try:
-        if current_pos[0] != 0 and maze.maze[current_pos[0]-1][current_pos[1]].wall:
-            try:
-                if maze.maze[current_pos[0]-2][current_pos[1]].wall and maze.maze[current_pos[0]-1][current_pos[1]-1].wall and maze.maze[current_pos[0]-1][current_pos[1]+1].wall:
-                    adj.append((current_pos[0]-1, current_pos[1]))
-            except:
-                adj.append((current_pos[0]-1, current_pos[1]))         
-    except:
-        pass
+def available(current, maze):
+        base = format(current.walls &~ current.edges, '04b')
+        new = 0b0000
 
-    try:
-        if current_pos[0] != maze.size-1 and maze.maze[current_pos[0]+1][current_pos[1]].wall:
-            try:
-                if maze.maze[current_pos[0]+2][current_pos[1]].wall and maze.maze[current_pos[0]+1][current_pos[1]-1].wall and maze.maze[current_pos[0]+1][current_pos[1]+1].wall:
-                    adj.append((current_pos[0]+1, current_pos[1]))
-            except:
-                adj.append((current_pos[0]+1, current_pos[1]))
-    except:
-        pass
+        if base[0] == "1" and not maze.maze[current.y-1][current.x].travelled:
+            new = new | 0b1000
+        if base[1] == "1" and not maze.maze[current.y][current.x+1].travelled:
+            new = new | 0b0100
+        if base[2] == "1" and not maze.maze[current.y+1][current.x].travelled:
+            new = new | 0b0010
+        if base[3] == "1" and not maze.maze[current.y][current.x-1].travelled:
+            new = new | 0b0001
 
-    try:
-        if current_pos[1] != 0 and maze.maze[current_pos[0]][current_pos[1]-1].wall:
-            try:
-                if maze.maze[current_pos[0]][current_pos[1]-2].wall and maze.maze[current_pos[0]-1][current_pos[1]-1].wall and maze.maze[current_pos[0]+1][current_pos[1]-1].wall:
-                    adj.append((current_pos[0], current_pos[1]-1))
-            except:
-                adj.append((current_pos[0], current_pos[1]-1))
-    except:
-        pass
+        return format(new, '04b')
 
-    try:    
-        if current_pos[1] != maze.size-1 and maze.maze[current_pos[0]][current_pos[1]+1].wall:
-            try:
-                if maze.maze[current_pos[0]][current_pos[1]+2].wall and maze.maze[current_pos[0]-1][current_pos[1]+1].wall and maze.maze[current_pos[0]+1][current_pos[1]+1].wall:
-                    adj.append((current_pos[0], current_pos[1]+1))
-            except:
-                adj.append((current_pos[0], current_pos[1]+1))
-    except:
-        pass
-
-    return adj
-
-def depth_first(maze, sx, sy):
-
-    maze.maze[sy][sx].next_point()
+def generate(maze, sx, sy):
 
     nodes = [maze.maze[sy][sx]]
-    
-    while len(nodes) != 0:
-        current_pos = (nodes[-1].y, nodes[-1].x)
-        available = adjacent(maze, current_pos)
-        visualise(maze)
-        input()
 
-        if len(available) == 0:
+    while len(nodes) != 0:
+        
+        current_node = nodes[-1]
+        current_node.travelled = True
+
+        next_direction = available(current_node, maze)
+        
+        if next_direction == "0000":
             nodes.pop()
         else:
-            current_pos = choice(available)
-            nodes.append(maze.maze[current_pos[0]][current_pos[1]])
-            nodes[-1].next_point()
+            direction = choice([x for x, i in enumerate(next_direction) if i == "1"])
+
+            if direction == 0: #north
+                current_node.walls = current_node.walls & 0b0111
+                other = maze.maze[current_node.y-1][current_node.x]
+                other.walls = other.walls & 0b1101
+
+            elif direction == 1: #east
+                current_node.walls = current_node.walls & 0b1011
+                other = maze.maze[current_node.y][current_node.x+1]
+                other.walls = other.walls & 0b1110
+
+            elif direction == 2: #south
+                current_node.walls = current_node.walls & 0b1101
+                other = maze.maze[current_node.y+1][current_node.x]
+                other.walls = other.walls & 0b0111
+
+            else: #west
+                current_node.walls = current_node.walls & 0b1110
+                other = maze.maze[current_node.y][current_node.x-1]
+                other.walls = other.walls & 0b1011
 
 
-def visualise(maze):
-    for i in maze.maze:
-        string = ""
-        for k in i:
-            if k.wall:
-                string += "O"
-            else:
-                string += "X"
-        print(string)
-        
+            nodes.append(other)
+        visualise(maze, current_node)
+    return maze
+
+
+#new stuff
+pygame.init()
+clock = pygame.time.Clock()
+white = (255, 255, 255)
+black = (0, 0, 0)
+display_width = 800
+display_height = 800
+window_gap = 100
+maze_width = display_width - window_gap*2     #For square maze and square window
+mazeDisp = pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption("aMazeing")
+mazeDisp.fill(white)
+pygame.display.update()
+
+#Displays a current state of the maze and displays it
+def visualise(maze, current_node):
+    mazeDisp.fill(white)
+    for row in maze.maze:
+        for node in row:
+            x = (maze_width/maze.size)*node.x + window_gap   #x and y position from top left corner of cell
+            y = (maze_width/maze.size)*node.y + window_gap
+            if node.walls & 0b1000:  #North
+                pygame.draw.line(mazeDisp, black, [x,y], [x+maze_width/maze.size, y], 1)
+            if node.walls & 0b0100:  #East
+                pygame.draw.line(mazeDisp, black, [x+maze_width/maze.size,y], [x+maze_width/maze.size, y+maze_width/maze.size], 1)
+            if node.walls & 0b0010:  #South
+                pygame.draw.line(mazeDisp, black, [x,y+maze_width/maze.size], [x+maze_width/maze.size, y+maze_width/maze.size], 1)
+            if node.walls & 0b0001:  #West
+                pygame.draw.line(mazeDisp, black, [x,y], [x, y+maze_width/maze.size], 1)
+            if current_node == node:
+                pygame.draw.rect(mazeDisp, (255,0,0), [x,y,maze_width/maze.size,maze_width/maze.size])
+    clock.tick(50)   #Controls speed of display
+    pygame.display.update()
+
+generate(Maze(50), 0, 0)
+print("debugg")
