5cc1710d3b985e0d4a0f586de5b2273b72e3a9fd
53942392 <53942392+Cai-Edwards@users.noreply.github.com>
2020-01-31 22:58:21 +0000

    Refactor + Features + Optimisations
    Optimised the depth first algorithm. Now greater than ~10x the speed it was before. It was mostly the drawing which was being done inefficiently. There are potential ways to increase the speed, for example joining connected lines together and just drawing it once. To do this perhaps wait until there is a break then render the new big line.
    
    ie) lastx, lasty. if not line, draw lastx, lasty, currentx, currenty. else continue.
    
    This would only work for horizontal lines though, and would have to add a seperate counter for the vertical ones.
    
    Need to add entrances.
    
    Added a solution visualiser (may not be the best path if random is used).
    
    Added starting and ending point options so you can choose where to start and end.
    
    Made the final screen resizeable such that the maze resizes with the screen.
    
    Can show or hide the solution with Return.
    
    When it reaches the end it goes back 3 nodes in the sequence to hopefully make the end more out of the way. ie only one path to it.
    
    Added a chance for a travelled node to be ignored, creating mazes with multiple solutions.
    
    Refactored the code to look and read nicer.
    
    Removed duplicate files.

--- depth_first.py
+++ depth_first.py
@@ -1,19 +1,20 @@
-from random import choice
+from random import choice, randint, random
+import pygame, time
 
 class Maze:
-
     def __init__(self, size):
         self.size = size
         self.maze = self.make_maze()
 
     def make_maze(self):
-
         maze = []
+        
         for y in range(self.size):
             maze.append([])
 
             for x in range(self.size):
                 edges = 0b0000
+                
                 if y == 0: edges = edges | 0b1000
                 if x == 0: edges = edges | 0b0001
                 if y == self.size-1: edges = edges | 0b0010
@@ -24,7 +25,6 @@ class Maze:
         return maze                
 
 class Node:
-
     def __init__(self, walls, edges, x, y):
         self.walls = walls
         self.edges = edges
@@ -33,23 +33,30 @@ class Node:
         self.y = y
 
 
-def available(current, maze):
+def available(current, maze, r):
         base = format(current.walls &~ current.edges, '04b')
         new = 0b0000
 
-        if base[0] == "1" and not maze.maze[current.y+1][current.x].travelled:
+        if base[0] == "1" and (not maze.maze[current.y-1][current.x].travelled or random() < r):
             new = new | 0b1000
-        if base[1] == "1" and not maze.maze[current.y][current.x+1].travelled:
+            
+        if base[1] == "1" and (not maze.maze[current.y][current.x+1].travelled or random() < r):
             new = new | 0b0100
-        if base[2] == "1" and not maze.maze[current.y-1][current.x].travelled:
+            
+        if base[2] == "1" and (not maze.maze[current.y+1][current.x].travelled or random() < r):
             new = new | 0b0010
-        if base[3] == "1" and not maze.maze[current.y][current.x-1].travelled:
+            
+        if base[3] == "1" and (not maze.maze[current.y][current.x-1].travelled or random() < r):
             new = new | 0b0001
 
         return format(new, '04b')
 
-def generate(maze, sx, sy):
+def generate(maze, sx, sy, ex, ey, show, r):
 
+    global display_width
+    global ratio
+    
+    ratio = round(display_width/maze.size-0.5)
     nodes = [maze.maze[sy][sx]]
 
     while len(nodes) != 0:
@@ -57,11 +64,18 @@ def generate(maze, sx, sy):
         current_node = nodes[-1]
         current_node.travelled = True
 
-        next_direction = available(current_node, maze)
-        print(next_direction)
+        if current_node.x == ex and current_node.y == ey:
+            path = [(z.x, z.y) for z in nodes]
+            nodes = nodes[:-3]
+            
+            current_node = nodes[-1]
+            current_node.travelled = True
+            
+        next_direction = available(current_node, maze, r)
         
         if next_direction == "0000":
             nodes.pop()
+            
         else:
             direction = choice([x for x, i in enumerate(next_direction) if i == "1"])
 
@@ -87,29 +101,118 @@ def generate(maze, sx, sy):
 
             nodes.append(other)
 
+        if show == True:
+            visualise_current(maze, current_node)
+
+    visualise_end(maze, sx, sy, ex, ey, path)
+    
     return maze
 
-def visualise(maze):
-    s = ""
-    s+="+" + "--+"*maze.size
+#Displays the ending state
+def visualise_current(maze, current_node):
+    global ratio
 
-    for i in maze.maze:
-        for k in i:
-            if k.walls & 0b0001:
-                s += "|"
-            else:
-                s + " "
+    d.fill(white)
+    for row in maze.maze:
+        for node in row:
             
-            s+= "  "
+            x = (ratio)*node.x
+            y = (ratio)*node.y
+            
+            if node.walls & 0b1000 and node.y == 0:  #North
+                pygame.draw.line(d, black, (x, y), (x + ratio, y), line_width)
+                
+            if node.walls & 0b0100:  #East
+                pygame.draw.line(d, black, (x + ratio, y), (x + ratio, y + ratio), line_width)
+                
+            if node.walls & 0b0010:  #South
+                pygame.draw.line(d, black, (x, y + ratio), (x + ratio, y + ratio), line_width)
+                
+            if node.walls & 0b0001 and node.x == 0:  #West
+                pygame.draw.line(d, black, (x, y), (x, y + ratio), line_width)
+                
+            if current_node == node:
+                pygame.draw.rect(d, red, (x, y, ratio, ratio))
+                
+    pygame.display.update()
+
+def visualise_end(maze, sx, sy, ex, ey, path):
+    global display_width
+    global display_height
+    show = False
+    d = pygame.display.set_mode((display_width, display_height), pygame.RESIZABLE)
+
+    
+    while True:
+        display_width = min(d.get_width(), d.get_height())
+        ratio = round(display_width/maze.size-0.5)
         
-        s += "\n|"
-        for k in i:
-            if k.walls & 0b0010:
-                s += "--"
-            else:
-                s += "  "
-            
-            s += "+"
-        s+="\n"
+        d.fill(white)
+        for row in maze.maze:
+            for node in row:
+                
+                x = (ratio)*node.x
+                y = (ratio)*node.y
+                
+                if node.walls & 0b1000 and node.y == 0:  #North
+                    pygame.draw.line(d, black, (x, y), (x + ratio, y), line_width)
+                    
+                if node.walls & 0b0100:  #East
+                    pygame.draw.line(d, black, (x + ratio, y), (x + ratio, y + ratio), line_width)
+                    
+                if node.walls & 0b0010:  #South
+                    pygame.draw.line(d, black, (x, y + ratio), (x + ratio, y + ratio), line_width)
+                    
+                if node.walls & 0b0001 and node.x == 0:  #West
+                    pygame.draw.line(d, black, (x, y), (x, y + ratio), line_width)
 
-    print(s)
+        pygame.draw.rect(d, green, (ratio*sx+line_width, ratio*sy+line_width, ratio - line_width, ratio - line_width))
+        pygame.draw.rect(d, green, (ratio*ex+line_width, ratio*ey+line_width, ratio - line_width, ratio - line_width))
+
+        if show:
+            new_path = [(round((z[0] + 0.5) * ratio), round((z[1] + 0.5) * ratio)) for z in path]
+            pygame.draw.lines(d, red, False, new_path, line_width)
+
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    
+                if event.key == pygame.K_RETURN:
+                    if show:
+                        show = False
+                    else:
+                        show = True
+                        
+            if event.type == pygame.VIDEORESIZE:
+                d = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+                
+if __name__ == "__main__":
+
+    s = 30 #Size of the maze
+    r = 0.00 #Chance of ANY travelled node beyond being ignored. ie the chance for one node to "break through" a wall is ~4r
+    k = True #Visualise the maze being made
+    
+    pygame.init()
+
+    clock = pygame.time.Clock()
+
+    white = (255, 255, 255)
+    red = (255, 0, 0)
+    green = (0, 255, 0)
+    black = (0, 0, 0)
+
+    line_width = 1
+
+    display_width = 800
+    display_height = 800
+
+    d = pygame.display.set_mode((display_width, display_height), pygame.RESIZABLE)
+    pygame.display.set_caption("Depth first maze generation")
+
+    generate(Maze(s), 15, 15, randint(0, s-1), randint(0, s-1), k, r)
+
